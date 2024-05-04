9b5fb9363364b47ef5742067f66f0610c876fbad
Sebastian Triemer <sebastian.triemer@outlook.de>
2024-05-04 11:17:18 +0200

    added 404 response for client

--- app/main.py
+++ app/main.py
@@ -31,14 +31,19 @@ def main():
         else:
             return None
         
-    print(get_http_path(http_header))
-        
-    #sendall has to be used here instead of send because send only sends as much data as possible within the sockets send buffer and then
-    #returns the number of bytes that were sent. So send needs to be called multiple times if it's buffer doesn't fit into the sockets send buffer.
-    #sendall on the other hand keeps sending data until all data from its buffer has been sent.
+    path = get_http_path(http_header)
+    if path == "/":
+        #sendall has to be used here instead of send because send only sends as much data as possible within the sockets send buffer and then
+        #returns the number of bytes that were sent. So send needs to be called multiple times if it's buffer doesn't fit into the sockets send buffer.
+        #sendall on the other hand keeps sending data until all data from its buffer has been sent.
 
-    #the b-prefix has to be used because the send methods expects bytes instead of strings.
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        #the b-prefix has to be used because the send methods expects bytes instead of strings.
+        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+        
+    
+   
 
 
 if __name__ == "__main__":
