29d8f8544b01d96365226880251a432564f72ff7
Sebastian Triemer <sebastian.triemer@outlook.de>
2024-05-04 09:58:02 +0200

    debugging

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen()
     conn = server_socket.accept() #this returns a new socket object, representing the connection to a client after 3-way handshake
+    print(conn)
     conn.send("HTTP/1.1 200 OK\r\n\r\n")
 
 
