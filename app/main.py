687d433c6071df9288de66b6c625b94b379b6fc1
Sebastian Triemer <sebastian.triemer@outlook.de>
2024-05-04 11:10:10 +0200

    testing if I can get the path

--- app/main.py
+++ app/main.py
@@ -10,8 +10,28 @@ def main():
     #this returns a tuple (conn, address) where conn is a new socket object, representing the connection to a client after 3-way handshake
     #and address is the clients address.
     conn, addr = server_socket.accept()
-    client_req = conn.recv(1024)
-    print (client_req.decode("utf-8"))
+    client_req = ""
+
+    #This loop ensures that the complete http header is received even if it contains more than 1024 byte.
+    #The http-header always ends with \r\n\r\n before the body starts
+    while True:
+        client_req += conn.recv(1024)
+        
+        if "\r\n\r\n" in client_req:
+            break
+
+    http_header = client_req.decode("utf-8")
+    print(get_http_path(http_header))
+
+    def get_http_path(http_header):
+        lines = http_header.split("\r\n")
+        first_line = lines[0]
+        words = first_line.split()
+        if len(words) >= 2:
+            return words[1]
+        else:
+            return None
+        
     #sendall has to be used here instead of send because send only sends as much data as possible within the sockets send buffer and then
     #returns the number of bytes that were sent. So send needs to be called multiple times if it's buffer doesn't fit into the sockets send buffer.
     #sendall on the other hand keeps sending data until all data from its buffer has been sent.
